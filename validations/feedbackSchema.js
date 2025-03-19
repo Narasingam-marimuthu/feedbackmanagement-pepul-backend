212c44b15c9e03bf09b179305e3d4093d59b7462
Narasingam-marimuthu <narasingam22@gmail.com>
2025-03-19 16:07:46 +0530

    validation issue fixed

--- validations/feedbackSchema.js
+++ validations/feedbackSchema.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const feedbackSchema = Joi.object({
   title: Joi.string().max(100).required(),
-  platform: Joi.string().valid('1', '2', '3').required(),
+  platform: Joi.string().required(),
   module: Joi.number().integer().required(),
   description: Joi.string().max(500).required(),
   attachment: Joi.string().uri().allow(null),
